809ba2af2884ba659407806b09baff8a2e90967f
Kiril <dygelniy.1994kirill@gmail.com>
2022-06-26 14:39:03 +0300

    comment ot our commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Todo from './component/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Learn React' />
+      <Todo text='Learn React' />
+      <Todo text='Learn React' />
+    </div>
+  );
+}
+
+export default App;
